94ab3649327bcfb59f2a337112012e45f1422664
Mohamed Yusuf <mohamedayusuf87@gmail.com>
2025-07-09 18:56:43 +0300

    Refactor code

--- tests/testthat/test-consistency_check.R
+++ tests/testthat/test-consistency_check.R
@@ -18,11 +18,11 @@ suppressMessages(
       # 1. Test when the length of tests and cases is not the same
       testthat::expect_error(
         consistency_check(dummy_data,
-                          tests = "malaria_rdt_test",
-                          cases = c(
-                            "malaria_rdt_cases",
-                            "malaria_micro_cases"
-                          )
+          tests = "malaria_rdt_test",
+          cases = c(
+            "malaria_rdt_cases",
+            "malaria_micro_cases"
+          )
         ),
         "The length of 'tests' and 'cases' must be the same."
       )
@@ -32,10 +32,10 @@ suppressMessages(
       cases_pass <- c("malaria_rdt_cases", "malaria_micro_cases")
 
       dummy_data2 <- dummy_data |>
-        dplyr::mutate(
-          malaria_rdt_test = abs(malaria_rdt_test) * 10000,
-          malaria_micro_test = abs(malaria_micro_test) * 10000
-        )
+      dplyr::mutate(
+        malaria_rdt_test = abs(malaria_rdt_test) * 10000,
+        malaria_micro_test = abs(malaria_micro_test) * 10000
+      )
 
       actual <- testthat::capture_messages(
         consistency_check(
@@ -49,123 +49,123 @@ suppressMessages(
         stringr::str_detect(
           actual,
           "Consistency test passed for malaria_rdt_test vs malaria_rdt_cases")
-      )
+        )
 
-      # 3. Test when some tests values are less than the cases values
-      dummy_data_with_inconsistency <- dummy_data
-      dummy_data_with_inconsistency$malaria_micro_test[1] <- 1000
+        # 3. Test when some tests values are less than the cases values
+        dummy_data_with_inconsistency <- dummy_data
+        dummy_data_with_inconsistency$malaria_micro_test[1] <- 1000
 
-      actual_inconsistency <-
+        actual_inconsistency <-
         testthat::capture_messages(
           consistency_check(dummy_data_with_inconsistency,
-                            tests = tests_pass, cases = cases_pass)
-        )[1]
+            tests = tests_pass, cases = cases_pass)
+          )[1]
 
-      testthat::expect_true(
-        stringr::str_detect(
-          actual_inconsistency,
-          "Consistency test failed for malaria_rdt_test vs malaria_rdt_cases")
+          testthat::expect_true(
+            stringr::str_detect(
+              actual_inconsistency,
+              "Consistency test failed for malaria_rdt_test vs malaria_rdt_cases")
+            )
+
+            # 4. Test the return type of the function
+            plot_result <- consistency_check(dummy_data,
+              tests = tests_pass,
+              cases = cases_pass
+            )
+
+            testthat::expect_equal(class(plot_result)[2], "ggplot")
+
+            # 5. Test with NA values
+            dummy_data_na <- dummy_data
+            dummy_data_na$malaria_rdt_test[1:10] <- NA
+            dummy_data_na$malaria_rdt_cases[11:20] <- NA
+
+            na_result <- consistency_check(dummy_data_na,
+              tests = tests_pass[1],
+              cases = cases_pass[1]
+            )
+
+            testthat::expect_s3_class(na_result, "ggplot")
+
+            # 7. Test with multiple disease types
+            multi_tests <- c("malaria_rdt_test", "dengue_test")
+            multi_cases <- c("malaria_rdt_cases", "dengue_cases")
+
+            multi_result <- consistency_check(dummy_data,
+              tests = multi_tests,
+              cases = multi_cases
+            )
+
+            testthat::expect_s3_class(multi_result, "ggplot")
+
+            # 8. Test saving functionality
+            # Create a temporary file path
+            temp_dir <- tempdir()
+
+            # Test saving the plot
+            save_result <- consistency_check(
+              dummy_data,
+              tests = tests_pass,
+              cases = cases_pass,
+              target_language = "fr",
+              save_plot = TRUE,
+              plot_path = temp_dir
+            )
+
+            # Check that the file was created
+            testthat::expect_true(file.exists(temp_dir))
+
+            # Check file size is greater than 0 (valid image)
+            testthat::expect_gt(file.size(temp_dir), 0)
+
+
+            # 11. Test directory creation functionality
+            # Create a temporary directory path
+            temp_dir <- file.path(tempdir(), "test_plots_dir", "nested_dir")
+
+            # Make sure the directory doesn't exist before the test
+            if (dir.exists(temp_dir)) {
+              unlink(temp_dir, recursive = TRUE)
+            }
+
+            # Test creating the directory
+            testthat::expect_false(dir.exists(temp_dir))
+            dir.create(temp_dir, recursive = TRUE)
+            testthat::expect_true(dir.exists(temp_dir))
+
+            # Test saving to the newly created directory
+            temp_file3 <- file.path(temp_dir, "test_plot.png")
+
+            save_dir_result <- consistency_check(dummy_data,
+              tests = tests_pass,
+              cases = cases_pass,
+              save_plot = TRUE,
+              plot_path = temp_file3
+            )
+
+            testthat::expect_true(file.exists(temp_file3))
+
+            # Test that recursive = FALSE fails when parent directory doesn't exist
+            non_existent_dir <- file.path(tempdir(), "non_existent", "test_dir")
+
+            if (dir.exists(non_existent_dir)) {
+              unlink(non_existent_dir, recursive = TRUE)
+            }
+
+            if (dir.exists(dirname(non_existent_dir))) {
+              unlink(dirname(non_existent_dir), recursive = TRUE)
+            }
+
+            # 12. Test error when save_plot is TRUE but plot_path is NULL
+            testthat::expect_error(
+              consistency_check(dummy_data,
+                tests = tests_pass,
+                cases = cases_pass,
+                save_plot = TRUE,
+                plot_path = NULL
+              ),
+              "plot_path must be provided when save_plot is TRUE."
+            )
+          })
+        )
       )
-
-      # 4. Test the return type of the function
-      plot_result <- consistency_check(dummy_data,
-                                       tests = tests_pass,
-                                       cases = cases_pass
-      )
-
-      testthat::expect_equal(class(plot_result)[2], "ggplot")
-
-      # 5. Test with NA values
-      dummy_data_na <- dummy_data
-      dummy_data_na$malaria_rdt_test[1:10] <- NA
-      dummy_data_na$malaria_rdt_cases[11:20] <- NA
-
-      na_result <- consistency_check(dummy_data_na,
-                                     tests = tests_pass[1],
-                                     cases = cases_pass[1]
-      )
-
-      testthat::expect_s3_class(na_result, "ggplot")
-
-      # 7. Test with multiple disease types
-      multi_tests <- c("malaria_rdt_test", "dengue_test")
-      multi_cases <- c("malaria_rdt_cases", "dengue_cases")
-
-      multi_result <- consistency_check(dummy_data,
-                                        tests = multi_tests,
-                                        cases = multi_cases
-      )
-
-      testthat::expect_s3_class(multi_result, "ggplot")
-
-      # 8. Test saving functionality
-      # Create a temporary file path
-      temp_dir <- tempdir()
-
-      # Test saving the plot
-      save_result <- consistency_check(
-        dummy_data,
-        tests = tests_pass,
-        cases = cases_pass,
-        target_language = "fr",
-        save_plot = TRUE,
-        plot_path = temp_dir
-      )
-
-      # Check that the file was created
-      testthat::expect_true(file.exists(temp_dir))
-
-      # Check file size is greater than 0 (valid image)
-      testthat::expect_gt(file.size(temp_dir), 0)
-
-
-      # 11. Test directory creation functionality
-      # Create a temporary directory path
-      temp_dir <- file.path(tempdir(), "test_plots_dir", "nested_dir")
-
-      # Make sure the directory doesn't exist before the test
-      if (dir.exists(temp_dir)) {
-        unlink(temp_dir, recursive = TRUE)
-      }
-
-      # Test creating the directory
-      testthat::expect_false(dir.exists(temp_dir))
-      dir.create(temp_dir, recursive = TRUE)
-      testthat::expect_true(dir.exists(temp_dir))
-
-      # Test saving to the newly created directory
-      temp_file3 <- file.path(temp_dir, "test_plot.png")
-
-      save_dir_result <- consistency_check(dummy_data,
-                                           tests = tests_pass,
-                                           cases = cases_pass,
-                                           save_plot = TRUE,
-                                           plot_path = temp_file3
-      )
-
-      testthat::expect_true(file.exists(temp_file3))
-
-      # Test that recursive = FALSE fails when parent directory doesn't exist
-      non_existent_dir <- file.path(tempdir(), "non_existent", "test_dir")
-
-      if (dir.exists(non_existent_dir)) {
-        unlink(non_existent_dir, recursive = TRUE)
-      }
-
-      if (dir.exists(dirname(non_existent_dir))) {
-        unlink(dirname(non_existent_dir), recursive = TRUE)
-      }
-
-      # 12. Test error when save_plot is TRUE but plot_path is NULL
-      testthat::expect_error(
-        consistency_check(dummy_data,
-                          tests = tests_pass,
-                          cases = cases_pass,
-                          save_plot = TRUE,
-                          plot_path = NULL
-        ),
-        "plot_path must be provided when save_plot is TRUE."
-      )
-    })
-  )
-)
